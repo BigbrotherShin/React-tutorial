1989b8d12d7aa2beb7dc2195cb3517e5c908dd01
BigbrotherShin <shinjhhp5@gmail.com>
2019-12-09 16:24:18 +0900

    use custom Hooks with reducer

--- src/useInputs.js
+++ src/useInputs.js
@@ -1,12 +1,39 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useReducer } from 'react';
+
+const reducer = (form, action) => {
+  switch (action.type) {
+    case 'ON_CHANGE':
+      return {
+        ...form,
+        [action.name]: action.value,
+      };
+    case 'RESET':
+      return action.initialForm;
+  }
+};
+
 
 function useInputs(initialForm) {
-  const [form, setForm] = useState(initialForm);
+  // const [form, setForm] = useState(initialForm);
+  const [form, dispatch] = useReducer(reducer, initialForm);
+
   const onChange = useCallback(e => {
     const { name, value } = e.target;
-    setForm(form => ({ ...form, [name]: value }));
+    // setForm(form => ({ ...form, [name]: value }));
+    dispatch({
+      type: 'ON_CHANGE',
+      name,
+      value,
+    });
   }, []);
-  const reset = useCallback(() => setForm(initialForm), [initialForm]);
+
+  const reset = useCallback(() => {
+    // setForm(initialForm), [initialForm]
+    dispatch({
+      type: 'RESET',
+      initialForm,
+    });
+  });
 
   return [form, onChange, reset];
 };
